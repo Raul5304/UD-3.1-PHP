f126d7fd80308126834e14c88bc272bf83f8b193
Raul5304 <raullecinena@iesrioarba.es>
2025-10-23 09:38:55 +0200

    .

---
+++ departamentos/form_dep2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Departamentos</title>
+</head>
+<body>
+    <a href="presupuesto.php?departamento=INFORMÁTICA">
+        Enviar datos de Informática
+    </a><br><br>
+    <a href="presupuesto.php?departamento=LENGUA">
+        Enviar datos de Lengua
+    </a><br><br>
+    <a href="presupuesto.php?departamento=MATEMÁTICAS">
+        Enviar datos de Matemáticas
+    </a><br><br>
+    <a href="presupuesto.php?departamento=INGLÉS">
+        Enviar datos de Inglés
+    </a>
+</body>
+</html>
--- departamentos/presupuesto.php
+++ departamentos/presupuesto.php
@@ -1,21 +1,18 @@
 <?php
-$department = $_REQUEST["departamento"];
+$department = $_GET["departamento"];
 
-switch($department){
-    case "INFORMÁTICA":
-        echo "$department = 500 euros";
-        break;
-    case "LENGUA":
-        echo "$department = 300 euros";
-        break;
-    case "MATEMÁTICAS":
-        echo "$department = 300 euros";
-        break;
-    case "INGLÉS":
-        echo "$department = 400 euros";
-        break;
+$presupuesto = [
+    "INFORMÁTICA" => 500,
+    "LENGUA" => 300,
+    "MATEMÁTICAS" => 300,
+    "INGLÉS" => 400
+];
+
+if (array_key_exists($department, $presupuesto)){
+    echo "El departamento $department tiene un presupuesto de " . $presupuesto[$department] . " euros";
+}else{
+    echo "El departamento seleccionado no existe";
 }
-
 ?>
 <br><br>
-<a href="form_dep.php">Volver al formulario</a>
+<a href="form_dep2.php">Volver al formulario</a>
