72a76beab46f2565850a24b6c8e65ffe85232af7
Raul5304 <raullecinena@iesrioarba.es>
2025-11-05 10:09:03 +0100

    .

--- listacompraJSONForm/nuevo.php
+++ listacompraJSONForm/nuevo.php
@@ -3,14 +3,11 @@ header("Content-Type: application/json; charset=UTF-8");
 
 $archivo = "compra.txt";
 
-// Recoger el nuevo artículo del formulario
 if (isset($_POST["articulo"]) && trim($_POST["articulo"]) !== "") {
     $nuevo = trim($_POST["articulo"]);
     
-    // Añadir al archivo
     file_put_contents($archivo, $nuevo . PHP_EOL, FILE_APPEND);
 }
 
-// Incluir listado.php para devolver la lista actualizada como JSON
 include("listado.php");
 ?>
